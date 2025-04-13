4b7f7a00154d404e3b77d0482244297139316497
J Reynolds <jreynolds@Isabels-MacBook-Pro-6.local>
2025-04-12 20:53:42 -0400

    cleaned up ccapture code removed code related to specific example

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import * as THREE from 'three';
-import { LineMaterial } from 'three/examples/jsm/Addons.js';
 import 'ccapture.js-npmfixed'
 
-let capturing = false
+const USE_CCAPTURE = true
+const USE_AUDIO = false
+const SONG_PATH = ''
 
-// let capturer = new CCapture({format: 'webm'})
+let capturing = USE_CCAPTURE
+let capturer = new CCapture({ format: 'webm' })
 
 // scene and renderer instantiation
 const scene = new THREE.Scene();
@@ -21,20 +23,90 @@ camera.add(listener)
 const sound = new THREE.Audio(listener)
 const analyser = new THREE.AudioAnalyser(sound, 32);
 const audioLoader = new THREE.AudioLoader();
-
-setTimeout(()=>{
-  audioLoader.load('sounds/Black Feathers Track 03.mp3', function (buffer) {
-    sound.setBuffer(buffer);
-    sound.setLoop(true);
-    sound.setVolume(0.5);  
-    sound.play()
-  });
-}, 3000)
-
-
 let frequencyArray = Array(16).fill(0)
 
-// capturer.start()
+if (USE_AUDIO) {
+  try {
+    setTimeout(() => {
+      audioLoader.load(SONG_PATH, function (buffer) {
+        sound.setBuffer(buffer);
+        sound.setLoop(true);
+        sound.setVolume(0.5);
+        sound.play()
+      });
+    }, 3000)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+function makeShape(shapeType, color, mesh = true) {
+  let geometry;
+  switch(shapeType) {
+    case "icosahedron":
+      geometry = new THREE.IcosahedronGeometry(1, 1, 1);
+      break;
+    case "cube":
+      geometry = new THREE.BoxGeometry(1, 1, 1);
+      break;
+    default:
+      break;
+  }
+
+  const material = mesh ? new THREE.MeshBasicMaterial({ color: color }) : new THREE.LineBasicMaterial({ color: color })
+  const shape = mesh ? new THREE.Mesh(geometry, material) : new THREE.Line(geometry, material);
+  return shape
+}
+
+function pulseShape(shape, frequencyIndex, direction = null) {
+  switch (direction) {
+    case "x":
+      shape.scale.x = 1 + frequencyArray[frequencyIndex] / 255
+      break;
+    case "y":
+      shape.scale.y = 1 + frequencyArray[frequencyIndex] / 255
+      break;
+    case "z":
+      shape.scale.z = 1 + frequencyArray[frequencyIndex] / 255
+      break;
+    default:
+      shape.scale.x = 1 + frequencyArray[frequencyIndex] / 255
+      shape.scale.y = 1 + frequencyArray[frequencyIndex] / 255
+      shape.scale.z = 1 + frequencyArray[frequencyIndex] / 255
+      break
+  }
+}
+
+function rotateShape(shape, x, y) {
+  shape.rotation.x += x;
+  shape.rotation.y += y;
+}
+
+function moveShape(shape, value, startDirection) {
+  shape.position[startDirection] += value
+}
+
+function initializeKeyListeners() {
+  window.addEventListener('keyup', (event) => {
+    switch (event.code) {
+      case "ArrowUp":
+        sound.setVolume(0.5)
+        break;
+      case "ArrowDown":
+        sound.setVolume(0)
+        break;
+      case "Space":
+        if (USE_CCAPTURE) {
+          if (capturing) {
+            capturer.stop()
+            capturer.save()
+            capturer.start()
+          }
+        }
+        break;
+    }
+  })
+}
 
 function animate() {
   requestAnimationFrame(animate)
@@ -42,76 +114,28 @@ function animate() {
    * Insert all the funky visual stuff in this loop
    */
 
-  cubes.forEach((cube, i) => {
-    pulseCube(cube, i)
-    if (i < 4) {
-      rotateCube(cube, .01, .01)
-    } else if (i < 8) {
-      rotateCube(cube, -.01, -.01)
-    } else {
-      rotateCube(cube, .01, -.01)
-    }
-  })
-
   renderer.render(scene, camera);
   frequencyArray = analyser.getFrequencyData()
-  // capturer.capture(renderer.domElement)
+
+  if (capturing) {
+    capturer.capture(renderer.domElement)
+  }
 }
 
-function makeCube(color, mesh=true) {
-  const geometry = new THREE.IcosahedronGeometry(1, 1, 1);
-  const material = mesh ? new THREE.MeshBasicMaterial({ color: color }) : new THREE.LineBasicMaterial({ color: color })
-  const cube = mesh ? new THREE.Mesh(geometry, material) : new THREE.Line(geometry, material);
-  return cube
-}
+// Initialize shapes etc
+let shapes = []
 
-function pulseCube(cube, frequencyIndex, direction=null) {
-  switch(direction) {
-    case "x":
-      cube.scale.x = 1 + frequencyArray[frequencyIndex] / 255
-      break;
-    case "y":
-      cube.scale.y = 1 + frequencyArray[frequencyIndex] / 255
-      break;
-    case "z":
-      cube.scale.z = 1 + frequencyArray[frequencyIndex] / 255
-      break;
-    default:
-      cube.scale.x = 1 + frequencyArray[frequencyIndex] / 255
-      cube.scale.y = 1 + frequencyArray[frequencyIndex] / 255
-      cube.scale.z = 1 + frequencyArray[frequencyIndex] / 255
-      break
-  }  
-}
-function rotateCube(cube, x, y) {
-  cube.rotation.x += x;
-  cube.rotation.y += y;
-}
-
-function moveCube(cube, value, startDirection) {  
-  cube.position[startDirection] += value
-}
-
-let cubes = Array(16).fill().map((e, i) => (
-  i < 4 ? makeCube(0xff0000, false) : i < 8 ? makeCube(0xffffff, false) : makeCube(0xff00ff, false)
-))
-
-cubes.forEach((cube) => {
-  scene.add(cube)
+shapes.forEach((shape) => {
+  scene.add(shape)
 })
 
 camera.position.z = 5;
 
+initializeKeyListeners()
+
+// start animation
 animate()
 
-window.addEventListener('keyup', (event) => {  
-  console.log(event.code)
-  switch(event.code) {
-    case "ArrowUp":
-      sound.setVolume(0.5)
-      break;
-    case "ArrowDown":
-      sound.setVolume(0)
-      break;
-  }  
-})
+if (USE_CCAPTURE) {
+  capturer.start()
+}
